863e51ba082e9e42c8aa52544595523b9ae7876c
PSurajSharma <127079867+PSurajSharma@users.noreply.github.com>
2024-06-12 17:47:27 +0530

    Update s3-zip.js

--- s3-zip.js
+++ s3-zip.js
@@ -1,5 +1,3 @@
-"use strict";
-
 require('aws-sdk/lib/maintenance_mode_message').suppress = true;
 
 const Client = require('ssh2-sftp-client');
@@ -8,11 +6,11 @@ const sftp = new Client();
 const AWS = require("aws-sdk");
 const stream = require("stream");
 const archiver = require("archiver");
-const request = require("request");
 const https = require("https");
-const path = require("path");
-const zlib = require('zlib');
+const lazystream = require("lazystream");
+const {app} = require("serverless/lib/cli/commands-schema/common-options/aws-service");
 const path = require('path');
+const {sleep} = require("ssh2-sftp-client/src/utils");
 const agent = new https.Agent({keepAlive: true, maxSockets: 16});
 
 AWS.config.update({httpOptions: {agent}, region: "eu-south-2"});
@@ -32,41 +30,36 @@ const transfer = async function sftpTransfer(sftpConfig, params, outputKey) {
 }
 
 const start = async function (inputBucket, inputDir, outputBucket, outputKey, format, context, callback) {
-    try {
-        if (!inputBucket || !outputBucket) {
-            throw new Error("Missing bucket name");
-        }
-
-        console.log(
-            `inputBucket: ${inputBucket}, outputBucket: ${outputBucket}, inputDir: ${inputDir}, outputKey: ${outputKey}, format : ${format}`
-        );
-
-        const outputFileName = outputKey + "." + format
-
-        let files;
-        if (inputBucket) {
-            files = await listObjects(inputBucket, inputDir);
-        }
-        if (!files || files.length === 0) {
-            throw new Error("Missing files, or inputDir is empty");
-        }
-
-        console.log(`input files size: ${files.length}`,files);
-
-        const batches = createBatches(files, parseInt(BATCH_SIZE, 10));
-        console.log("number of batches", batches.length)
-        for (let i = 0; i < batches.length; i++) {
-            await uploadBatch(batches[i], i, inputBucket, outputBucket, inputDir, outputKey, format);
-        }
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({outputFileName}),
-        };
-    } catch (error) {
-        console.error("Start error:", error);
-        throw error;
+    if (!inputBucket || !outputBucket) {
+        throw new Error("Missing bucket name");
     }
+
+    console.log(
+        `inputBucket: ${inputBucket}, outputBucket: ${outputBucket}, inputDir: ${inputDir}, outputKey: ${outputKey}, format : ${format}`
+    );
+
+    const outputFileName = outputKey + "." + format
+
+    let files;
+    if (inputBucket) {
+        files = await listObjects(inputBucket, inputDir);
+    }
+    if (!files || files.length === 0) {
+        throw new Error("Missing files, or inputDir is empty");
+    }
+
+    console.log(`input files size: ${files.length}`);
+
+    const batches = createBatches(files, parseInt(BATCH_SIZE, 10));
+    console.log("number of batches", batches.length)
+    for (let i = 0; i < batches.length; i++) {
+        await uploadBatch(batches[i], i, inputBucket, outputBucket, inputDir, outputKey, format);
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({outputFileName}),
+    };
 }
 
 const createBatches = (files, batchSize) => {
@@ -86,82 +79,84 @@ const uploadBatch = async (files, batchIndex, inputBucket, outputBucket, inputDi
         ContentType: "application/zip",
         Key: batchFileName,
         Bucket: outputBucket,
-        ServerSideEncryption: "AES256"
     };
 
-    try {
-        const s3Upload = s3.upload(uploadParams);
+    const s3Upload = s3.upload(uploadParams, (err) => {
+        if (err) {
+            console.error("upload error", err);
+        } else {
+            console.log("upload done");
+        }
+    });
 
-        const archive = archiver(format, {
-            zlib: {level: 0},
-        });
-        archive.on("error", (error) => {
-            throw new Error(
-                `${error.name} ${error.code} ${error.message} ${error.path}  ${error.stack}`
+    const s3FileDownloadStreams = files.map((file) => {
+        return {
+            stream: new lazystream.Readable(() => {
+                return s3
+                    .getObject({Bucket: inputBucket, Key: file.key})
+                    .createReadStream();
+            }),
+            fileName: file.fileName,
+        };
+    });
+
+    const archive = archiver(format, {
+        zlib: {level: 0},
+    });
+    archive.on("error", (error) => {
+        throw new Error(
+            `${error.name} ${error.code} ${error.message} ${error.path}  ${error.stack}`
+        );
+    });
+
+    archive.on("progress", (progress) => {
+        let batchSize = parseInt(BATCH_SIZE, 10);
+        let logKey = batchSize
+        if (batchSize > 100) {
+            logKey = 100
+        }
+        if (progress.entries.processed % logKey === 0) {
+            console.log(
+                `archive ${batchFileName} progress: ${progress.entries.processed} / ${progress.entries.total}`
             );
-        });
+        }
+    });
 
-        archive.on("progress", (progress) => {
-            let batchSize = parseInt(BATCH_SIZE, 10);
-            let logKey = batchSize
-            if (batchSize > 100) {
-                logKey = 100
-            }
-            if (progress.entries.processed % logKey === 0) {
-                console.log(
-                    `archive ${batchFileName} progress: ${progress.entries.processed} / ${progress.entries.total}`
-                );
-            }
-        });
+    s3Upload.on("httpUploadProgress", (progress) => {
+        if (progress.loaded % (1024 * 1024) === 0) {
+            console.log(`upload ${outputKey}, loaded size: ${progress.loaded}`);
+            console.log(
+                `memory usage: ${process.memoryUsage().heapUsed / 1024 / 1024} MB`
+            );
+        }
+    });
 
-        s3Upload.on("httpUploadProgress", (progress) => {
-            if (progress.loaded % (1024 * 1024) === 0) {
-                console.log(`upload ${outputKey}, loaded size: ${progress.loaded}`);
-                console.log(
-                    `memory usage: ${process.memoryUsage().heapUsed / 1024 / 1024} MB`
-                );
-            }
-        });
+    await new Promise(async (resolve, reject) => {
+        streamPassThrough.on("close", () => onEvent("close", resolve));
+        streamPassThrough.on("end", () => onEvent("end", resolve));
+        streamPassThrough.on("error", () => onEvent("error", reject));
 
         archive.pipe(streamPassThrough);
-        for (const ins of files) {
-            const fileName = ins.fileName;
+        s3FileDownloadStreams.forEach((ins) => {
             if (batchFileName === ins.fileName || ins.fileName === (inputDir + "/") || ins.fileName === "/") {
                 console.warn(`skipping file: ${ins.fileName}`);
                 // skip the output file, may be duplicating zip files
                 return;
             }
-            console.log("Appending file to archive:", fileName);
-            const s3Stream = getStream(inputBucket, ins.key);
-            archive.append(s3Stream, { name: fileName });
-        }
-        archive.finalize();
+            archive.append(ins.stream, {name: ins.fileName});
+        });
 
-        console.log("Upload done");
-        await s3Upload.promise();
-    } catch (error) {
-        console.error("Upload batch error:", error);
-        throw error;
-    }
-};
-
-const getStream = (bucket, key) => {
-    let streamCreated = false;
-    const passThroughStream = new stream.PassThrough();
-
-    passThroughStream.on("newListener", event => {
-        if (!streamCreated && event === "data") {
-            const s3Stream = s3
-                .getObject({ Bucket: bucket, Key: key })
-                .createReadStream();
-            s3Stream
-                .on("error", err => passThroughStream.emit("error", err))
-                .pipe(passThroughStream);
-
-            streamCreated = true;
-        }
+        await sleep(2000)
+        await archive.finalize();
+    }).catch((error) => {
+        throw new Error(`${error.code} ${error.message} ${error.data}`);
     });
-    return passThroughStream;
+    s3FileDownloadStreams.forEach((ins) => {
+        ins.stream.end()
+    })
+    console.log("Upload done");
+    await s3Upload.promise();
+    streamPassThrough.end()
 }
 
 const listObjects = async (bucket, prefix) => {
